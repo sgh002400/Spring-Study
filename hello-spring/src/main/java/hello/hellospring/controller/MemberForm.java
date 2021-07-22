ab698381f67f01af82ec37edfc4b6163d1c5dbb2
jihwan <sgh002400@naver.com>
2021-07-22 23:36:28 +0900

    간단한 회원 가입 및 회원 목록 보여주기 실습

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
