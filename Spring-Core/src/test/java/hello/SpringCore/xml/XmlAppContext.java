ffa5f0e21078410110570ccc3c465c97d537d725
jihwan <sgh002400@naver.com>
2021-07-25 17:40:50 +0900

    Xml로 AppConfig 작성해보기

---
+++ Spring-Core/src/test/java/hello/SpringCore/xml/XmlAppContext.java
@@ -0,0 +1,18 @@
+package hello.SpringCore.xml;
+
+import hello.SpringCore.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
