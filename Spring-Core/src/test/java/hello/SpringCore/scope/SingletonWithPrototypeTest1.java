104c682e9967e265077939373a0042ebe1d540a4
jihwan <sgh002400@naver.com>
2021-07-27 16:44:55 +0900

    싱클톤 빈과 프로토타입 스코프를 함께 사용시 문제점 해결(ObjectProvider 사용)

--- Spring-Core/src/test/java/hello/SpringCore/scope/SingletonWithPrototypeTest1.java
+++ Spring-Core/src/test/java/hello/SpringCore/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,7 @@
 package hello.SpringCore.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -21,20 +22,21 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
-        //주입 시점에 스프링 컨테이너에 요청해서 프로토타입 빈이 새로 생성이 된 것이지,
-        //사용 할 때마다 새로 생성되는 것이 아니기 때문에 원하는 결과와 다르게 나오는 것을 확인할 수 있다.
+        assertThat(count2).isEqualTo(1);
+
     }
 
+    @Scope("singleton") //Default긴함
     static class ClientBean {
 
-        private final PrototypeBean prototypeBean;
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            //실행해보면 prototypeBeanProvider.getObject() 을 통해서 항상 새로운 프로토타입 빈이 생성되는 것을 확인할 수 있다.
+            //ObjectProvider 의 getObject() 를 호출하면 내부에서는 스프링 컨테이너를 통해 해당 빈(여기선 프로토타입 빈)을 찾아서 반환한다. (DL)
+
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
