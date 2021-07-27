7356a4cd9279462ffded9cab22b809dfa6850d76
jihwan <sgh002400@naver.com>
2021-07-27 18:54:44 +0900

    Requset 스코프 Practice - Provider를 사용한 에러 해결

--- Spring-Core/src/main/java/hello/SpringCore/web/LogDemoController.java
+++ Spring-Core/src/main/java/hello/SpringCore/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.SpringCore.web;
 
 import hello.SpringCore.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,13 +14,14 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;//지금 실행시키면 오류가 난다! -> 스프링 컨테이너가 뜨는 시점에는 Http 요청이 없는데 요청을 했기 때문이다!
-    // -> @Provider로 해결 가능 (인터셉터를 모른다 가정한 예시임.)
+    private final ObjectProvider<MyLogger> myLoggerProvider; //MyLogger를 주입 받는게 아니라 이걸 찾을 수 있는 애(DL)가 주입된다.
 
     @RequestMapping("log-demo")
     @ResponseBody //문자를 그대로 응답 보낼 수 있게 해줌.
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+
+        MyLogger myLogger = myLoggerProvider.getObject(); //진짜 필요한 이 시점에 주입을 받아 준다!(객체가 만들어진다.)
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
--- Spring-Core/src/main/java/hello/SpringCore/web/LogDemoService.java
+++ Spring-Core/src/main/java/hello/SpringCore/web/LogDemoService.java
@@ -2,6 +2,7 @@ package hello.SpringCore.web;
 
 import hello.SpringCore.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject(); //이 시점에 객체가 만들어진다.
         myLogger.log("service id = " + id);
     }
 }
